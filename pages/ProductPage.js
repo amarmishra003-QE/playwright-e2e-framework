dac9bd371e2381f0448d50cbf8f4061e58e0f05b
amarmishra003-QE <amarmishra003@gmail.com>
2026-04-02 16:56:33 +0530

    Update ProductPage.js

--- pages/ProductPage.js
+++ pages/ProductPage.js
@@ -1,1 +1,12 @@
 
+class ProductPage {
+  constructor(page) {
+    this.page = page;
+  }
+
+  async open() {
+    await this.page.goto('https://example.com');
+  }
+}
+
+module.exports = { ProductPage };
