b2df2819f57e3049f17ce8100ea70a9b05ecd518
amarmishra003-QE <amarmishra003@gmail.com>
2026-04-02 16:52:22 +0530

    Add login button selector to LoginPage class

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,10 +1,15 @@
 class LoginPage {
   constructor(page) {
     this.page = page;
+    this.loginButton = 'text=Login';
   }
 
   async open() {
     await this.page.goto('https://example.com');
+  }
+
+  async clickLogin() {
+    await this.page.click(this.loginButton);
   }
 }
 
