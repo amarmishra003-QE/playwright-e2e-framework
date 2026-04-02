fb63dcfc9ec1671059febbea21d3826b7ca3e40f
amarmishra003-QE <amarmishra003@gmail.com>
2026-04-02 16:55:49 +0530

    Add checkout flow test using Playwright

--- tests/checkout.spec.js
+++ tests/checkout.spec.js
@@ -1,1 +1,8 @@
 
+const { test, expect } = require('@playwright/test');
+
+test('Checkout Flow', async ({ page }) => {
+  await page.goto('https://example.com');
+
+  await expect(page).toBeTruthy();
+});
